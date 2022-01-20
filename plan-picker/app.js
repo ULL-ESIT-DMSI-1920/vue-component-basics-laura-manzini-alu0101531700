108602495573c87ba418441d3d27ff3dd8c6feb2
lauramanzini <232566@studenti.unimore.it>
2022-01-20 18:41:25 +0000

    Created structure for plan-picker

---
+++ plan-picker/app.js
@@ -0,0 +1,18 @@
+<div class="plans">
+    <div class="plan">
+        <div class="description">
+        <span class="title">
+            The Single
+        </span>
+        </div>
+    </div>
+
+    <div class="plan">
+        <div class="description">
+        <span class="title">
+            The Addict
+        </span>
+        </div>
+    </div>
+    
+</div>
